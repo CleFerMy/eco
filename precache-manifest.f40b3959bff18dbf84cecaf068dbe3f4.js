f1d5e56486099f04ba0c0b0ae3605fa7555a6953
CleFer <clefermy@gmail.com>
2019-02-27 19:49:53 +0300

    Updates

---
+++ precache-manifest.f40b3959bff18dbf84cecaf068dbe3f4.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f795caae8945f8a874d81df55d4a3f0",
+    "url": "/eco/static/media/c.0f795caa.svg"
+  },
+  {
+    "revision": "886cd53df24a46aaec8f",
+    "url": "/eco/static/css/main.48837c57.chunk.css"
+  },
+  {
+    "revision": "e3776e3d0bde82cb4d0b",
+    "url": "/eco/static/js/runtime~main.e3776e3d.js"
+  },
+  {
+    "revision": "887410be6d35538b95c3889811d5ac35",
+    "url": "/eco/static/media/b.887410be.jpg"
+  },
+  {
+    "revision": "d6751c81629ef902cfe0",
+    "url": "/eco/static/js/2.d6751c81.chunk.js"
+  },
+  {
+    "revision": "0955caab9d5796325900f0c0a0f1f37b",
+    "url": "/eco/static/media/s.0955caab.png"
+  },
+  {
+    "revision": "5d64416fbd89691fb2d773a9e30c60f9",
+    "url": "/eco/static/media/circle.5d64416f.svg"
+  },
+  {
+    "revision": "ca3cb1e8181e7e24c84fa2fdd471b26b",
+    "url": "/eco/static/media/cross.ca3cb1e8.svg"
+  },
+  {
+    "revision": "7852d2a201d3aea0d6a647ee9c769e30",
+    "url": "/eco/static/media/home.7852d2a2.png"
+  },
+  {
+    "revision": "149491d29d4b7fd2378008ac7f432968",
+    "url": "/eco/static/media/kn.149491d2.png"
+  },
+  {
+    "revision": "e5101751c6eaa943f88b2996be524c67",
+    "url": "/eco/static/media/h.e5101751.svg"
+  },
+  {
+    "revision": "d84f765d71797309e6b9ac34542d90c8",
+    "url": "/eco/static/media/d.d84f765d.svg"
+  },
+  {
+    "revision": "886cd53df24a46aaec8f",
+    "url": "/eco/static/js/main.886cd53d.chunk.js"
+  },
+  {
+    "revision": "5819e3e9d0d98ddf3e2823fe23882ed2",
+    "url": "/eco/static/media/s.5819e3e9.svg"
+  },
+  {
+    "revision": "b1ee7ffe9705e7c7dff1e72a3da04ad0",
+    "url": "/eco/static/media/bank.b1ee7ffe.svg"
+  },
+  {
+    "revision": "66bb2d019e7a886541a7a5b1fc9c3ba9",
+    "url": "/eco/static/media/job.66bb2d01.svg"
+  },
+  {
+    "revision": "e1b5570ae0f558e15d1b0b854e171bb5",
+    "url": "/eco/static/media/home.e1b5570a.svg"
+  },
+  {
+    "revision": "51cb987cde77e6c3795368eb49a649ab",
+    "url": "/eco/static/media/game.51cb987c.svg"
+  },
+  {
+    "revision": "ba249130a43fa836436cd03023ea5732",
+    "url": "/eco/static/media/shop.ba249130.svg"
+  },
+  {
+    "revision": "0b79e7d2b8912f8f8a0bfd35ae23b91f",
+    "url": "/eco/static/media/lefto.0b79e7d2.svg"
+  },
+  {
+    "revision": "fc3b0f97b6617f9624cfccc41c9e4ca5",
+    "url": "/eco/static/media/righto.fc3b0f97.svg"
+  },
+  {
+    "revision": "d7b47ea0f2aedfbdb291d89795b8e933",
+    "url": "/eco/static/media/HoylePlayingCards.d7b47ea0.ttf"
+  },
+  {
+    "revision": "d6751c81629ef902cfe0",
+    "url": "/eco/static/css/2.6c2e6fe9.chunk.css"
+  },
+  {
+    "revision": "4f3584b72f2a5e2ea6acb07f16280565",
+    "url": "/eco/index.html"
+  }
+];
